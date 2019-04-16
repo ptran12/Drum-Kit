195629f50302bb5aa4a0d755831e1c11d2afbc01
Phi Tran <phitran@Phis-MBP.attlocal.net>
2019-04-15 22:30:42 -0500

    sound added

---
+++ index.js
@@ -0,0 +1,17 @@
+var drumBtn = document.querySelectorAll(".drum").length;
+
+
+
+console.log(drumBtn);
+
+for (var i = 0; i < drumBtn; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+
+    });
+}
+
+
+ 
